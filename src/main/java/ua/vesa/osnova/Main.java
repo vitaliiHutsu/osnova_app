b04882db89106fddce318ce4c42475ecfcc77847
vitaliiHutsu <vitalii.hutsu@gmail.com>
2016-11-28 23:07:17 +0200

    test

--- src/main/java/ua/vesa/osnova/Main.java
+++ src/main/java/ua/vesa/osnova/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println(System.getProperty("os.name").substring(0, 7).toLowerCase());
-        System.out.println("Hello World");
 
     }
 }
